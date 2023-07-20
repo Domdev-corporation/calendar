7d1a47fb0d90f5e6a632fc81963833c5cde20956
Oleksandr <didyshen.oleksandr@gmail.com>
2023-07-20 16:07:41 +0300

    Remove boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,4 @@
-import ReactDOM from 'react-dom/client'
 import './styles/index.css'
-
 import Chronous from './Chronous'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <div className="paper">
-    <Chronous
-      view="Week"
-      //   config={[{ maxWidth: 850, mode: 'Week' }]}
-      onClickCell={console.log}
-      onClickEvent={console.log}
-      selectedEvent="98f6c30c-a95e-4794-b2ee-5c0143cbe0b5"
-    />
-  </div>,
-)
+export default Chronous
