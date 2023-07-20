ba63d3824637a60374362860798f6dde202ece83
IgorKozlovsky <i.kozlovsky2004@gmail.com>
2023-07-20 16:56:57 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,4 @@
-import ReactDOM from 'react-dom/client'
-
 import './styles/index.css'
-import Calendar from './features/Chronous'
+import Chronous from './features/Chronous'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <div className="paper">
-    <Calendar
-      mode="mobile"
-      onClickCell={console.log}
-      newEventModal={console.log}
-      onClickEvent={console.log}
-      selectedEvent="98f6c30c-a95e-4794-b2ee-5c0143cbe0b5"
-    />
-  </div>,
-)
+export default Chronous
